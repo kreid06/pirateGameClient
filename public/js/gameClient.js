169bdec18fce29c9ec2531dc17d13d52eebed0e6
kreid06 <kevinreid3light@gmail.com>
2025-01-17 16:37:36 -0500

    mouseControls

--- public/js/gameClient.js
+++ public/js/gameClient.js
@@ -32,6 +32,8 @@ class GameClient {
         
         this.lastFrameTime = 0;
         this.lastUpdateTime = 0;
+        this.mousePos = { x: 0, y: 0 };
+        this.setupMouseTracking();
         this.gameLoop();
     }
 
@@ -51,30 +53,49 @@ class GameClient {
         });
     }
 
+    setupMouseTracking() {
+        window.addEventListener('mousemove', (e) => {
+            this.mousePos.x = e.clientX;
+            this.mousePos.y = e.clientY;
+            this.updateRotation();
+        });
+    }
+
+    updateRotation() {
+        // Calculate angle between player and cursor
+        const dx = this.mousePos.x - MovementData.VIEW_CENTER.x;
+        const dy = this.mousePos.y - MovementData.VIEW_CENTER.y;
+        this.rotation = Math.atan2(dy, dx);
+    }
+
     updateWorldPosition() {
         let moved = false;
         const oldX = this.worldPos.x;
         const oldY = this.worldPos.y;
 
-        // Check current key states
-        if (this.keys['ArrowUp'] || this.keys['KeyW']) {
-            this.worldPos.y -= this.speed;
+        // Movement direction based on rotation
+        if (this.keys['KeyW']) { // Forward
+            this.worldPos.x += Math.cos(this.rotation) * this.speed;
+            this.worldPos.y += Math.sin(this.rotation) * this.speed;
             moved = true;
         }
-        if (this.keys['ArrowDown'] || this.keys['KeyS']) {
-            this.worldPos.y += this.speed;
+        if (this.keys['KeyS']) { // Backward
+            this.worldPos.x -= Math.cos(this.rotation) * this.speed;
+            this.worldPos.y -= Math.sin(this.rotation) * this.speed;
             moved = true;
         }
-        if (this.keys['ArrowLeft'] || this.keys['KeyA']) {
-            this.worldPos.x -= this.speed;
+        if (this.keys['KeyA']) { // Strafe Left
+            this.worldPos.x += Math.sin(this.rotation) * this.speed;
+            this.worldPos.y -= Math.cos(this.rotation) * this.speed;
             moved = true;
         }
-        if (this.keys['ArrowRight'] || this.keys['KeyD']) {
-            this.worldPos.x += this.speed;
+        if (this.keys['KeyD']) { // Strafe Right
+            this.worldPos.x -= Math.sin(this.rotation) * this.speed;
+            this.worldPos.y += Math.cos(this.rotation) * this.speed;
             moved = true;
         }
 
-        // Send movement data if position changed
+        // Update movement if position changed
         if (moved) {
             this.movement = new MovementData(
                 this.worldPos.x,
@@ -83,8 +104,6 @@ class GameClient {
                 this.rotation,
                 this.playerId
             );
-            
-            // Send to server
             this.connection.sendMovementData(
                 this.worldPos.x,
                 this.worldPos.y,
@@ -132,18 +151,18 @@ class GameClient {
         // Clear canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         
-        // Draw grid first
+        // Draw grid
         this.movement.drawGrid(this.ctx);
         
-        // Draw player centered
+        // Draw rotated player
         this.ctx.save();
+        this.ctx.translate(MovementData.VIEW_CENTER.x, MovementData.VIEW_CENTER.y);
+        this.ctx.rotate(this.rotation);
+        
+        // Draw square centered at origin
         this.ctx.fillStyle = 'red';
-        this.ctx.fillRect(
-            MovementData.VIEW_CENTER.x - 10,
-            MovementData.VIEW_CENTER.y - 10,
-            20,
-            20
-        );
+        this.ctx.fillRect(-10, -10, 20, 20);
+        
         this.ctx.restore();
     }
 
